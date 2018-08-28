2cc8bedc86d26006c1e36685edda1952c842b640
Jim Brighter <jbrighter92@gmail.com>
2018-08-28 13:47:05 -0400

    prod cors config

---
+++ planner-svc/src/main/java/planner/config/LocalCorsConfig.java
@@ -0,0 +1,34 @@
+package planner.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@Profile("default")
+public class LocalCorsConfig {
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		List<String> origins = new ArrayList<String>();
+		origins.add("http://localhost:4200");
+		List<String> methods = new ArrayList<String>();
+		methods.add("GET");
+		methods.add("POST");
+		methods.add("OPTIONS");
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(origins);
+		config.setAllowedMethods(methods);
+		config.addAllowedHeader("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+}
