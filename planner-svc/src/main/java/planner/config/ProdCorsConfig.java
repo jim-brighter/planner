c0dce28a364a230d725ed1c25bba0619bea216bf
Jim Brighter <jbrighter92@gmail.com>
2018-08-28 14:34:33 -0400

    log cors config

--- planner-svc/src/main/java/planner/config/ProdCorsConfig.java
+++ planner-svc/src/main/java/planner/config/ProdCorsConfig.java
@@ -3,6 +3,8 @@ package planner.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,9 +15,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @Profile("prod")
 public class ProdCorsConfig {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProdCorsConfig.class); 
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
+		logger.info("Loading prod CORS config");
 		List<String> origins = new ArrayList<String>();
 		origins.add("http://jimandfangzhuo.com");
 		List<String> methods = new ArrayList<String>();
