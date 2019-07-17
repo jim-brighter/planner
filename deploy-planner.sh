afcf329b7b55856d8e755b2433acc22d4627f1d2
Jim Brighter <jbrighter92@gmail.com>
2019-07-17 19:52:52 -0400

    update image id

--- deploy-planner.sh
+++ deploy-planner.sh
@@ -27,7 +27,7 @@ log "Launching the new droplet"
 doctl compute droplet create $NEW_DROPLET \
 --region nyc3 \
 --size s-1vcpu-1gb \
---image 47146497 \
+--image 48822839 \
 --ssh-keys 22134471,23526912,24637185 \
 --enable-monitoring \
 --tag-names $NEW_DROPLET \
